3e194b50bd14d60cc5fe7b900b67048177c60457
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-05-06 18:00:56 +0700

    chore: limit song name

--- model/Music.php
+++ model/Music.php
@@ -7,7 +7,7 @@ class Music extends Model
 
     public function limitName(): string
     {
-        return substr($this->name, 0, 20).(strlen($this->name) > 20 ? '...' : '');
+        return substr($this->name, 0, 15).(strlen($this->name) > 15 ? '...' : '');
     }
 
 }
